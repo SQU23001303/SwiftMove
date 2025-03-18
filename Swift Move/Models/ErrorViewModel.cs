8ca584f41a3c485d0371c3d732a206f0451fdf5d
SQU23001303 <157488287+SQU23001303@users.noreply.github.com>
2025-03-18 10:52:05 +0000

    Add project files.

---
+++ Swift Move/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Swift_Move.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
