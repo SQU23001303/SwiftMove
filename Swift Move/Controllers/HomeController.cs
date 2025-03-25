fe79560b37bdffe9c1526cfe924902e40a396d89
SQU23001303 <157488287+SQU23001303@users.noreply.github.com>
2025-03-25 15:44:00 +0000

    Booking form now works

--- Swift Move/Controllers/HomeController.cs
+++ Swift Move/Controllers/HomeController.cs
@@ -1,16 +1,20 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Swift_Move.Data;
 using Swift_Move.Models;
 
 namespace Swift_Move.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -53,5 +57,33 @@ namespace Swift_Move.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ServiceModel service)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    service.CollectionDate = service.CollectionDate.ToUniversalTime();
+                    service.DeliveryDate = service.DeliveryDate.ToUniversalTime();
+
+                    _context.Service.Add(service);
+                    _context.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "An error occurred while saving the service. Please try again.");
+                }
+            }
+
+            return View(service);
+        }
+
+
+
     }
 }
---
+++ Swift Move/Controllers/ServiceController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swift_Move.Data;
+using Swift_Move.Models;
+
+public class ServiceController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public ServiceController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+   
+
+
+}
